f2f20f62586e93507f0c6259e42511f5915cd852
Gyana Ranjan <gyanaranjansahoo174@gmail.com>
2026-03-24 23:07:57 +0530

    Fix plane animation to start from the absolute left edge

--- hawk/src/App.js
+++ hawk/src/App.js
@@ -12,7 +12,7 @@ function App() {
           if (entry.isIntersecting) {
             entry.target.classList.add('show');
           } else {
-            entry.target.classList.remove('show ');
+            entry.target.classList.remove('show');
           }
         });
       },
